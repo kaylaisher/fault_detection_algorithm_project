992b77788ed687ee63c6785ef198ea8fb19c1313
kaylaisher <147053990+kaylaisher@users.noreply.github.com>
2025-11-05 14:10:27 +0800

    Create user_entry.py

---
+++ user_entry.py
@@ -0,0 +1,4 @@
+# user can use this module with python user_entry.py | fee log.txt
+# call config and engine and plug in selected algorithm
+# call fault coverage calculator
+# could be interactive (still have default selection to demo)
